68cd733a385563da141c9f95a6d12be7d16a0775
nguyenngoctiengit <56434615+nguyenngoctiengit@users.noreply.github.com>
2021-02-17 16:58:41 +0700

    17/2

--- pmkd/Controllers/HopdongController.cs
+++ pmkd/Controllers/HopdongController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using pmkd.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,20 @@ namespace pmkd.Controllers
 {
     public class HopdongController : Controller
     {
+        public tradingsystem_blContext _context;
+        public tradingsystem_blContext db = new tradingsystem_blContext();
+        public HopdongController(tradingsystem_blContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult hdmb()
+        {
+            ViewBag.mucung = (from a in _context.Hdmbs join b in _context.PortfolioPayments on a.Thanhtoan equals b.Matt select b.Mucung).ToList();
+            return View("hdmb",_context.Hdmbs.ToList());
         }
     }
 }
