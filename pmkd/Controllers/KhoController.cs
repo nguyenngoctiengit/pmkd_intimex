c2688ec3d47d7ba0179103c7ff55bed1779e1673
nguyenngoctiengit <56434615+nguyenngoctiengit@users.noreply.github.com>
2021-05-12 16:49:55 +0700

    12/5

--- pmkd/Controllers/KhoController.cs
+++ pmkd/Controllers/KhoController.cs
@@ -150,29 +150,8 @@ namespace pmkd.Controllers
                         transaction.Rollback();
                         return BadRequest("Error occurred" + ex);
                     }
-                }
-                 
-                
+                }  
             }
-/*            if (xeptai.Kcs == "")
-            {
-                var autoincrement_kcs = _context.AutomaticValuesBranches.Where(a => a.Macn == "INXBL" && a.ObjectName == "KCSNNLBLI").FirstOrDefault();
-                var PrefixOfDefaultValueForId_kcs = autoincrement_kcs.PrefixOfDefaultValueForId;
-                var LengthOfDefaultValueForId_kcs = (int)autoincrement_kcs.LengthOfDefaultValueForId;
-                var LastValueOfColumnId_kcs = autoincrement_kcs.LastValueOfColumnId;
-                var oldValue_kcs = LastValueOfColumnId_kcs.Substring(6, 6);
-                var oldValueInt_kcs = Convert.ToInt32(oldValue_kcs);
-                var currentValue_kcs = oldValueInt_kcs + 1;
-                var nextValue_kcs = oldValueInt_kcs + 2;
-                var chuoi_kcs = "00000000" + Convert.ToString(currentValue_kcs);
-                var chuoinext_kcs = "00000000" + Convert.ToString(nextValue_kcs);
-                var parameterOut_kcs = PrefixOfDefaultValueForId_kcs + chuoi_kcs.Substring(chuoi_kcs.Length + PrefixOfDefaultValueForId_kcs.Length - LengthOfDefaultValueForId_kcs, 7);
-                var next_kcs = PrefixOfDefaultValueForId_kcs + chuoinext_kcs.Substring(chuoinext_kcs.Length + PrefixOfDefaultValueForId_kcs.Length - LengthOfDefaultValueForId_kcs, 7);
-                xeptai.Kcs = parameterOut_kcs;
-                autoincrement_kcs.LastValueOfColumnId = parameterOut_kcs;
-                autoincrement_kcs.NextValueOfColumnId = next_kcs;
-                _context.AutomaticValuesBranches.Update(autoincrement_kcs);
-            }*/
             if (!TryValidateModel(xeptai))
                 return BadRequest(GetFullErrorMessage(ModelState));
 
@@ -329,9 +308,88 @@ namespace pmkd.Controllers
         [Route("kho/kho/themkcs")]
         public IActionResult themkcs()
         {
+            ViewBag.name = HttpContext.Session.GetString("FullName1");
             ViewBag.xeptai = _context.XepTais.ToList();
-            ViewBag.aaa = _context.KhachHangs.ToList();
+            ViewBag.can = _context.Cans.ToList();
             return View("kcs/themkcs");
+        }
+        [HttpPost]
+        public IActionResult themkcs1(Kc kc)
+        {
+            if (_context.Kcs.Where(a => a.XeptaiId == kc.XeptaiId).FirstOrDefault() != null)
+            {
+                TempData["alertMessage"] = "Xe đã được lập KCS, vui lòng kiểm tra lại";
+                return RedirectToAction("themkcs");
+            }
+            using (var transaction = _context.Database.BeginTransaction())
+            {
+                try
+                {
+                    Kc kc1 = new Kc();
+                    XepTai xepTai = new XepTai();
+                    var autoincrement_kcs = _context.AutomaticValuesBranches.Where(a => a.Macn == "INXBL" && a.ObjectName == "KCSNNLBLI").FirstOrDefault();
+                    var PrefixOfDefaultValueForId_kcs = autoincrement_kcs.PrefixOfDefaultValueForId;
+                    var LengthOfDefaultValueForId_kcs = (int)autoincrement_kcs.LengthOfDefaultValueForId;
+                    var LastValueOfColumnId_kcs = autoincrement_kcs.LastValueOfColumnId;
+                    var oldValue_kcs = LastValueOfColumnId_kcs.Substring(6, 6);
+                    var oldValueInt_kcs = Convert.ToInt32(oldValue_kcs);
+                    var currentValue_kcs = oldValueInt_kcs + 1;
+                    var nextValue_kcs = oldValueInt_kcs + 2;
+                    var chuoi_kcs = "00000000" + Convert.ToString(currentValue_kcs);
+                    var chuoinext_kcs = "00000000" + Convert.ToString(nextValue_kcs);
+                    var parameterOut_kcs = PrefixOfDefaultValueForId_kcs + chuoi_kcs.Substring(chuoi_kcs.Length + PrefixOfDefaultValueForId_kcs.Length - LengthOfDefaultValueForId_kcs, 7);
+                    var next_kcs = PrefixOfDefaultValueForId_kcs + chuoinext_kcs.Substring(chuoinext_kcs.Length + PrefixOfDefaultValueForId_kcs.Length - LengthOfDefaultValueForId_kcs, 7);
+                    xepTai.Kcs = parameterOut_kcs;
+                    autoincrement_kcs.LastValueOfColumnId = parameterOut_kcs;
+                    autoincrement_kcs.NextValueOfColumnId = next_kcs;
+                    kc1.SoPhieu = parameterOut_kcs;
+                    kc1.NguoiGiao = kc.KhachHang;
+                    var xevc = (from a in _context.XepTais where a.Id == kc.XeptaiId select a.SoXe).FirstOrDefault();
+                    kc1.XeVc = xevc;
+                    kc1.KhachHang = kc.KhachHang;kc1.DoAm = kc.DoAm;kc1.TapChat = kc.TapChat;kc1.DenVo = kc.DenVo;kc1.HatMoc = kc.HatMoc;
+                    kc1.HatNau = kc.HatNau;kc1.HatCxk = kc.HatCxk;kc1.TrangXop = kc.TrangXop;kc1.HatChay = kc.HatChay;
+                    kc1.Sang8 = kc.Sang8; kc1.Sang12 = kc.Sang12; kc1.Sang13 = kc.Sang13; kc1.Sang14 = kc.Sang14; kc1.Sang15 = kc.Sang15; 
+                    kc1.Sang16 = kc.Sang16; kc1.Sang17 = kc.Sang17; kc1.Sang18 = kc.Sang18; kc1.Sang19 = kc.Sang19; kc1.Sang20 = kc.Sang20;
+                    kc1.Ghichu = kc.Ghichu;
+                    kc1.Manhom = kc.Manhom;
+                    kc1.NgayNhap = DateTime.Now;
+                    kc1.NguoiKiem = kc.NguoiKiem;
+                    kc1.LoaiKcs = 1;
+                    kc1.SoLuong = kc.SoLuong;
+                    kc1.PhuTrach = kc.PhuTrach;
+                    kc1.NguonHang = kc.NguonHang;
+                    kc1.Dvt = "kgs";
+                    var canid = (from a in _context.XepTais where a.Id == kc.XeptaiId select a.CanId).FirstOrDefault();
+                    var khoid = (from a in _context.XepTais where a.Id == kc.XeptaiId select a.KhoId).FirstOrDefault();
+                    kc1.CanId = canid;
+                    kc1.KhoId = khoid;
+                    kc1.TrongLuongNw = kc.TrongLuongNw;
+                    kc1.NguoiLayMau = kc.NguoiLayMau;
+                    kc1.XeptaiId = kc.XeptaiId;
+                    kc1.Macn = HttpContext.Session.GetString("UnitName");
+                    kc1.NguoiTao = HttpContext.Session.GetString("userId");
+                    kc1.NgayPhieu = DateTime.Now;
+                    kc1.TenKhach = kc.TenKhach;
+                    kc1.LoaiBao = kc.LoaiBao;
+                    kc1.TrongLuong = kc.TrongLuong;
+                    kc1.Mahang = kc.Mahang;
+                    kc1.TenHang = kc.TenHang;
+                    _context.AutomaticValuesBranches.Update(autoincrement_kcs);
+                    _context.XepTais.Update(xepTai);
+                    _context.Kcs.Add(kc1);
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    TempData["alertMessage"] = ex;
+                    return RedirectToAction("themkcs");
+                }
+            }
+            _context.SaveChanges();
+            TempData["alertMessage"] = "thêm KCS thành công";
+            return RedirectToAction("kcs");
+
         }
     }
 }
--- pmkd/Startup.cs
+++ pmkd/Startup.cs
@@ -49,6 +49,7 @@ namespace pmkd
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddDbContext<tradingsystem_blContext>(ServiceLifetime.Transient);
             services.AddMvc();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
