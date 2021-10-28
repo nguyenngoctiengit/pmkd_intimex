e1ecb2e585469f6b07a6463a6d525af980a734c0
nguyenngoctiengit <56434615+nguyenngoctiengit@users.noreply.github.com>
2021-10-28 16:36:22 +0700

    28/10

--- Intimex_project/Controllers/DocComeController.cs
+++ Intimex_project/Controllers/DocComeController.cs
@@ -3,6 +3,7 @@ using Data.Models.Trading_system;
 using DevExtreme.AspNet.Data;
 using DevExtreme.AspNet.Mvc;
 using DevExtreme.AspNet.Mvc.FileManagement;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -26,19 +27,47 @@ namespace Intimex_project.Controllers
         [HttpGet]
         public async Task<IActionResult>  Get(DataSourceLoadOptions loadOptions)
         {
-            var item_return = _context.Documents.Select(i => new
-            {
-                i.DocId,
-                i.DocDate,
-                i.DateCome,
-                i.NumberCome,
-                i.Contents,
-                i.DocTypeId,
-                i.NumberSign,
-                i.DocPlaceId,
-                i.DocLever,
-            });
+            var item_return = (from a in _context.Documents
+                               select new
+                               {
+                                   DocId = a.DocId,
+                                   DocDate = a.DocDate,
+                                   DateCome = a.DateCome,
+                                   NumberCome = a.NumberCome,
+                                   Contents = a.Contents,
+                                   DocTypeId = a.DocTypeId,
+                                   NumberSign = a.NumberSign,
+                                   DocPlaceId = a.DocPlaceId,
+                                   DocLever = a.DocLever,
+                                   IsChuyen = _context.DocProcesses.Count(b => b.DocId == a.DocId) > 0 ? true : false
+                               });
             return Json(await DataSourceLoader.LoadAsync(item_return, loadOptions));
+        }
+        public IActionResult AddDocCome()
+        {
+            return View("AddDocCome");
+        }
+        public async Task<IActionResult> add_doccome(Document document)
+        {
+            Document _document = new Document();
+            _document.DateCreate = DateTime.Now;
+            _document.DocLever = document.DocLever;
+            _document.NumberCome = document.NumberCome;
+            _document.DocDate = document.DocDate;
+            _document.NumberOfPage = document.NumberOfPage;
+            _document.NumberSign = document.NumberSign;
+            _document.DateCome = document.DateCome;
+            _document.DocTypeId = document.DocTypeId;
+            _document.DocPlaceId = document.DocPlaceId;
+            _document.Contents = document.Contents;
+            _document.Note = document.Note;
+            _document.Singer = "";
+            _document.UserCreate = HttpContext.Session.GetString("userId");
+            _document.Macn = HttpContext.Session.GetString("UnitName");
+            await _context.Documents.AddAsync(_document);
+            await _context.SaveChangesAsync();
+            TempData["alertMessage"] = "thêm văn bản đến thành công";
+            return RedirectToAction("DocCome");
         }
     }
 }
---
+++ Intimex_project/wwwroot/js/js_create/doccome.js
@@ -0,0 +1,12 @@
+﻿function toolbar_DocCome(e) {
+    e.toolbarOptions.items.unshift({
+        location: "after",
+        widget: "dxButton",
+        options: {
+            icon: "add",
+            onClick: function () {
+                location.href = "/doccome/AddDocCome";
+            }
+        }
+    });
+}
