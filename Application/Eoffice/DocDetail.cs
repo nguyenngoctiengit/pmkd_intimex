a3fdb1859e5cd92388c4c1f0d641c22ad5778069
nguyenngoctiengit <56434615+nguyenngoctiengit@users.noreply.github.com>
2021-12-27 17:10:27 +0700

    27/12

---
+++ Application/Eoffice/DocDetail.cs
@@ -0,0 +1,32 @@
+﻿using Application.Parameter;
+using Data.Models.Trading_system;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Eoffice
+{
+    public class DocDetail
+    {
+        public static bool CheckUserViewImage(long DocId,string UserName)
+        {
+            tradingsystemContext _context = new tradingsystemContext(ConnectionParameter.connectionString);
+            var Sp = "exec sp_Document;31 " +
+                        "@DocId = '" + DocId + "'," +
+                        "@UserName = '" + UserName + "'";
+            var item = _context.Documents.FromSqlRaw(Sp).ToList();
+            if (item == null || item.Count == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            
+        }
+    }
+}
--- Intimex_project/Controllers/DocDetailsController.cs
+++ Intimex_project/Controllers/DocDetailsController.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Eoffice;
 
 namespace Intimex_project.Controllers
 {
@@ -31,6 +32,49 @@ namespace Intimex_project.Controllers
                         "@User = '"+ HttpContext.Session.GetString("UserName") + "'";
             var item = _context.Sp_GetDocDetails.FromSqlRaw(Sp).ToList();
             return DataSourceLoader.Load(item, loadOptions);
+        }
+        [HttpPost]
+        public IActionResult DisplayNameReceive(long DocId,long IsFeedBack)
+        {
+            if (IsFeedBack == 0)
+            {
+                var listUserReceive = (from a in _context.DocProcesses join b in _context.UserRights on a.ObjectProcess equals b.UserName1
+                                       join c in _context.Departments on b.Department equals c.DepartmentId where a.DocId == DocId select new {
+                                           b.FullName1,
+                                           c.DepartmentCode
+
+                                       }).ToList();
+                return Json(listUserReceive);
+            }
+            else
+            {
+                var listUserReceive = (from a in _context.DocProcesses
+                                       join b in _context.DocFeedBacks on a.DocProcessId equals b.DocProcessId
+                                       join c in _context.UserRights on b.UserReceive equals c.UserName1
+                                       join d in _context.Departments on c.Department equals d.DepartmentId where a.DocId == DocId && b.UserSend == HttpContext.Session.GetString("UserName")
+                                       select new {
+
+                                           c.FullName1,
+                                           d.DepartmentCode
+
+                                       }).ToList();
+                return Json(listUserReceive);
+            }
+            
+        }
+        [HttpPost]
+        public IActionResult ViewImage(long DocId)
+        {
+
+            if (DocDetail.CheckUserViewImage(DocId, HttpContext.Session.GetString("UserName")) == true)
+            {
+                ViewBag.ListImage = _context.DocFileAttaches.Where(a => a.DocId == DocId).ToList();
+                return PartialView("_PartiView_ViewFileDocument");
+            }
+            else
+            {
+                return Json("false");
+            }
         }
     }
 }
