e57e95198e3a9572ef476f4f51aa3cbca46db493
nguyenngoctiengit <56434615+nguyenngoctiengit@users.noreply.github.com>
2022-02-16 16:35:26 +0700

    16/02

--- Intimex_project/Controllers/ChungtuController.cs
+++ Intimex_project/Controllers/ChungtuController.cs
@@ -1,16 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Parameter;
+using Data.Models.Trading_system;
+using DevExtreme.AspNet.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Data.Public_class;
+using Microsoft.AspNetCore.Http;
+using DevExtreme.AspNet.Data;
 
 namespace Intimex_project.Controllers
 {
     public class ChungtuController : Controller
     {
-        public IActionResult Index()
+        private readonly tradingsystemContext _context = new tradingsystemContext(ConnectionParameter.connectionString);
+        public IActionResult AddVoucher()
         {
-            return View();
+            return View("AddVoucher");
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetChungtu(DataSourceLoadOptions loadOptions)
+        {
+            var ChungTu = _context.Hdmbs.Where(a => a.Trangthai != 2 && a.MuaBan == "BAN" && a.Macn == HttpContext.Session.GetString("UnitName")).Select(i => new {
+                i.Ref,
+                i.Sohd,
+            }).OrderBy(a => a.Sohd);
+            return Json(await DataSourceLoader.LoadAsync(ChungTu, loadOptions));
         }
     }
 }
