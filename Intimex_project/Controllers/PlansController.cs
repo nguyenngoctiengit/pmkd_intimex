1f3dd53c7b310dabcbcb6f900179ff0e019d644b
nguyenngoctiengit <56434615+nguyenngoctiengit@users.noreply.github.com>
2022-04-05 16:45:03 +0700

    05/04

---
+++ Intimex_project/Controllers/PlansController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intimex_project.Controllers
+{
+    public class PlansController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddPlans(string id)
+        {
+            return View("AddPlans");
+        }
+        public IActionResult AddPlans_Bootstrap(string id)
+        {
+            return View("_Content_bootstrap_Plans");
+        }
+    }
+}
