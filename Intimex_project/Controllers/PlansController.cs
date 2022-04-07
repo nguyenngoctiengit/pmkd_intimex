e3e75452d186746280d1ca97ef66253e0f4c523a
nguyenngoctiengit <56434615+nguyenngoctiengit@users.noreply.github.com>
2022-04-07 16:21:02 +0700

    07/04

--- Intimex_project/Controllers/PlansController.cs
+++ Intimex_project/Controllers/PlansController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Parameter;
+using Data.Models.Trading_system;
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +12,7 @@ namespace Intimex_project.Controllers
 {
     public class PlansController : Controller
     {
+        private readonly tradingsystemContext _context = new tradingsystemContext(ConnectionParameter.connectionString);
         public IActionResult Index()
         {
             return View();
@@ -16,9 +21,56 @@ namespace Intimex_project.Controllers
         {
             return View("AddPlans");
         }
-        public IActionResult AddPlans_Bootstrap(string id)
+        [HttpGet]
+        public object getPlans(string id,DataSourceLoadOptions loadOptions)
         {
-            return View("_Content_bootstrap_Plans");
+            var item = (from a in _context.PairedPlans
+                        join b in _context.Hdmbs on a.ContracId equals b.Systemref
+                        where a.PlansId == id && b.MuaBan == "MUA"
+                        select new
+                        {
+                            a.PlansId,
+                            a.ContracId,
+                            a.SystemId,
+                            a.Trongluong,
+                            a.Macn,
+                            b.Systemref,
+                            b.Ref,
+                            b.Sohd,
+                            b.Trangthai,
+                            b.MuaBan,
+                            b.Makhach,
+                            b.Ngayky,
+                            b.Ngaygiao,
+                            b.Ngayhl,
+                            b.Ngaytl,
+                            b.Nguoitl,
+                            b.Nguoilam,
+                            b.Ghichu,
+                            b.Pakd,
+                            b.SoPakd,
+                            b.IsFix,
+                            b.Tiente,
+                            b.ThanhtoanId,
+                            b.Thanhtoan,
+                            b.Ngaylam,
+                            b.IntKy,
+                            b.ClientKy,
+                            b.Docstatus,
+                            b.TrangthaiGhep,
+                            b.TienUngHd,
+                            b.TienUngTt,
+                            b.HdcmuonId,
+                            b.SoHdcmuon,
+                            b.DiaDiemGiaoHang,
+                            b.IsNoKhoDoi,
+                            b.TypeKd,
+                            b.VanChuyen,
+                            b.NgayTraPhaitra,
+                            b.Tenfull
+                        }).ToList();
+            return DataSourceLoader.Load(item, loadOptions);
         }
+
     }
 }
